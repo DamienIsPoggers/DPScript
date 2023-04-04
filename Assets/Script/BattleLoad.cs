99f974dfd31017f456dbafd324fb4843c524e13d
Damien McCormick <DMcCormick20172017@outlook.com>
2023-04-03 20:55:34 -0700

    Somewhat got loading working

--- Assets/Script/BattleLoad.cs
+++ Assets/Script/BattleLoad.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using ImGuiNET;
+using System.Text;
 
 public class BattleLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Animator camera;
+    byte[] sceneLoad = new byte[255];
+
+    void OnEnable()
     {
-        
+        ImGuiUn.Layout += Draw;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        
+        ImGuiUn.Layout -= Draw;
+    }
+
+    void Draw()
+    {
+        if(ImGui.Begin("LoadMenu"))
+        {
+            ImGui.InputText("Scene to load", sceneLoad, 255);
+            if (ImGui.Button("Load"))
+                StartCoroutine(battleLoad(Encoding.ASCII.GetString(sceneLoad).Replace("\0", string.Empty), "", ""));
+            ImGui.End();
+        }
+    }
+
+    public IEnumerator battleLoad(string sceneToLoad, string p1, string p2)
+    {
+        AsyncOperation scene = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Single);
+        scene.allowSceneActivation = false;
+
+        while (scene.progress <= 0.9)
+            yield return null;
+
+        camera.Play("CameraDone");
+        while (camera.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1)
+            yield return null;
+
+        scene.allowSceneActivation = true;
     }
 }
